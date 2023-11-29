0539040a9064b08be1e87e90e6392109d884d06e
snehadasoji <sneha.dasoji@gmail.com>
2023-11-29 12:22:03 -0500

    11/29/2023

---
+++ src/test/java/sneha/First.java
@@ -0,0 +1,22 @@
+
+
+package sneha;
+public class First {
+
+    //Sum of 10 integer number
+
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum=sum+i;
+
+
+        }
+
+        System.out.println("Sum of :" +sum);
+    }
+}
+
+
+
